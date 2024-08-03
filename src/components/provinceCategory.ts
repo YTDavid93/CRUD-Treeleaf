e1ff23c1e9881dd0b8c30c600f9857d1d5267fae
YTDavid93 <ytd3809@gmail.com>
2024-08-03 17:53:07 +0545

    defualt category for provinnce

---
+++ src/components/provinceCategory.ts
@@ -0,0 +1,12 @@
+
+const provinceCategory = [
+  "Province 1",
+  "Madhesh Pradesh",
+  "Bagmati Province",
+  "Gandaki",
+  "Lumbini",
+  "Karnali",
+  "Sudurpaschim",
+];
+
+export default provinceCategory;
