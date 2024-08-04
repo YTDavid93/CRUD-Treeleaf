dcf57357e54622ce4335fdbe5cfcd118745a96b1
YTDavid93 <ytd3809@gmail.com>
2024-08-04 11:36:18 +0545

    build userform component

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,10 +1,50 @@
 import { useEffect, useState } from "react";
 import provinceCategory from "./provinceCategory";
 import countryName, { CountryName } from "../services/countryName";
+import ErrorMessage from "../utils/ErrorMessage";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-const UserForm = () => {
-  const { setValue } = useForm();
+interface Props {
+  onSubmit: (data: FormData) => void;
+}
+
+const ACCEPTED_IMAGE_TYPES = ".image/png";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be 3 characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  number: z.string().regex(/^\d{7,10}$/, {
+    message: "Phone number must be between 7 and 10 digits.",
+  }),
+  date: z.string().min(1, { message: "Date is required"}),
+  city: z.string().min(1, { message: "City is required." }),
+  district: z.string().min(1, { message: "District is required." }),
+  province: z.enum(provinceCategory, {
+    errorMap: () => ({ message: "Province is required" }),
+  }),
+  country: z.string(),
+  picture: z
+    .any()
+    .refine((files) => files?.length >= 1, { message: "Image is required." })
+    .refine(
+      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+      "Only .png formats are supported."
+    ),
+});
+
+export type FormData = z.infer<typeof schema>;
+
+const UserForm = ({ onSubmit }: Props) => {
+  const {
+    register,
+    setValue,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
   const [countries, setCountries] = useState<CountryName[]>([]);
   const [selectedCountry, setSelectedCountry] = useState<string>("");
@@ -21,7 +61,7 @@ const UserForm = () => {
         );
         if (nepal) {
           setSelectedCountry(nepal.name.common);
-          setValue("name", nepal.name.common);
+          setValue("country", nepal.name.common);
         }
       })
       .catch((err) => {
@@ -37,96 +77,104 @@ const UserForm = () => {
   };
 
   return (
-    <section>
-      <form>
-        <div>
+    <section className="flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="p-6 bg-white rounded drop-shadow-lg"
+      >
+        <div className="mb-3">
           <label
             htmlFor="name"
-            // className="block text-sm font-medium text-gray-700"
           >
             Name <span className="text-red-500">*</span>
           </label>
           <input
+            {...register("name")}
             type="text"
             id="name"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
         </div>
 
-        <div>
+        <div className="mb-3">
           <label htmlFor="email">
             Email
             <span className="text-red-500">*</span>
           </label>
           <input
-            // {...register("email")}
+            {...register("email")}
             type="text"
             id="email"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </div>
 
-        <div>
+        <div className="mb-3">
           <label
             htmlFor="number"
-            // className="block text-sm font-medium text-gray-700"
           >
             Phone Number <span className="text-red-500">*</span>
           </label>
           <input
-            // {...register("number")}
+            {...register("number")}
             type="text"
             id="number"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.number && (
+            <ErrorMessage>{errors.number.message}</ErrorMessage>
+          )}
         </div>
 
-        <div>
+        <div className="mb-3">
           <label
             htmlFor="date"
-            // className="block text-sm font-medium text-gray-700"
           >
             DOB
           </label>
           <input
-            // {...register("date")}
+            {...register("date")}
             type="date"
             id="date"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
+          {errors.date && <ErrorMessage>{errors.date.message}</ErrorMessage>}
         </div>
 
-        <div>
+        <div className="mb-3">
           <label className="block text-sm font-medium text-gray-700">
             City
           </label>
           <input
-            // {...register("city")}
+            {...register("city")}
             type="text"
             id="city"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
-          {/* {errors.city && <ErrorMessage>{errors.city.message}</ErrorMessage>} */}
+          {errors.city && <ErrorMessage>{errors.city.message}</ErrorMessage>}
         </div>
 
-        <div>
+        <div className="mb-3">
           <label className="block text-sm font-medium text-gray-700">
             District
           </label>
           <input
-            // {...register("district")}
+            {...register("district")}
             type="text"
             id="district"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           />
-          {/* {errors.district && (
+          {errors.district && (
             <ErrorMessage>{errors.district.message}</ErrorMessage>
-          )} */}
+          )}
         </div>
 
-        <div>
+        <div className="mb-3">
           <label htmlFor="province">Province</label>
           <select
+            {...register("province")}
             id="province"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
           >
@@ -137,9 +185,12 @@ const UserForm = () => {
               </option>
             ))}
           </select>
+          {errors.province && (
+            <ErrorMessage>{errors.province.message}</ErrorMessage>
+          )}
         </div>
 
-        <div>
+        <div className="mb-3">
           <label
             htmlFor="country"
             className="block text-sm font-medium text-gray-700"
@@ -147,7 +198,7 @@ const UserForm = () => {
             Country
           </label>
           <select
-            // {...register("country")}
+            {...register("country")}
             id="country"
             className="w-full px-3 py-2 rounded-md border focus:outline-none focus:ring focus:border-blue-300"
             value={selectedCountry}
@@ -159,14 +210,20 @@ const UserForm = () => {
               </option>
             ))}
           </select>
-          {/* {errors.country && (
+          {error && <ErrorMessage>{error}</ErrorMessage>}
+          {errors.country && (
             <ErrorMessage>{errors.country.message}</ErrorMessage>
-          )} */}
+          )}
         </div>
 
-        <div>
+        <div className="mb-3">
           <label htmlFor="picture">Profile Picture</label>
-          <input type="file" id="picture" onChange={handleChange} />
+          <input
+            {...register("picture")}
+            type="file"
+            id="picture"
+            onChange={handleChange}
+          />
           {file && (
             <img
               src={file}
@@ -174,8 +231,12 @@ const UserForm = () => {
               className="w-[100px] h-[100px] rounded-[50%] object-cover"
             />
           )}
+          {errors.picture && (
+            <ErrorMessage>{errors.picture.message as string}</ErrorMessage>
+          )}
         </div>
-        <button className="bg-blue-500 rounded-lg text-white text-base w-full px-4 py-2 hover:bg-white hover:text-customBlue hover:border hover:border-customBlue transition-all duration-200 ease-in mt-3">
+
+        <button className="bg-blue-500 rounded-lg text-white text-base w-full px-4 py-2 hover:bg-white hover:text-customBlue hover:border hover:border-customBlue transition-all duration-200 ease-in-out">
           Save
         </button>
       </form>
